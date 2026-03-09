3ef9c979d2c76c747acab74df866e1de1ba21df8
Kyur05 <kyurmaharaj@gmail.com>
2026-03-09 10:39:34 +0200

    Initial clean commit for road project

---
+++ app.py
@@ -0,0 +1,7 @@
+print("---Welcome to your first Python App---")
+#This stores what you type onto a variable called 'user name'
+user_name=input("What is your name?")
+#This stores your favourite number
+fav_number=input("What is your favourite number?")
+#This prints it all together
+print(f"Cool name,{user_name}!If we double your favourite number, it would be {int(fav_number)*2}.")
